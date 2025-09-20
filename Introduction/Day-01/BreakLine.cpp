53f204a06ac55b03558fbce7cc77b88989cee8a0
Jitu Vishwakarma <jiturajsharma8093@gmail.com>
2025-09-20 15:51:39 +0530

    some important notes and code

---
+++ Introduction/Day-01/BreakLine.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;    
+
+int main(){
+    cout<<"Hello Pw";
+    cout<<endl;
+    cout<<endl;
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"Hellow CW"<<endl<<"Second rule";
+
+}
---
+++ Introduction/Day-01/printNumber.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<5<<endl; 
+    cout<<4+54<<endl;
+    cout<<"43+43"<<endl;
+    cout<<65-43<<endl;
+
+}
